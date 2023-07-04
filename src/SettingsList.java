e0149c41beadad81975e2b5881295baeb178f575
M1jdNo <majdnoameh@gmail.com>
2023-07-04 21:03:11 +0200

    veränderung

--- src/SettingsList.java
+++ src/SettingsList.java
@@ -26,12 +26,25 @@ public class SettingsList extends Observable {
         }
     }
 
+    ///////////////////CLOSE FEHLT NOCH!!!!!!!!!!!!!!!
+
     public String getterMethode(String typ) {
         String pfad = null;
         System.out.println("awdawdawdawd");
         try (Connection connection = ds.getConnection();
+
+             //Ist der Treiber geladen und die Verbindung hergestellt,
+              //  benötigen Sie ein Objekt der Klasse Statement zum Absetzen von SQL-Kommandos
+
              Statement statement = connection.createStatement();
+
+           //  Mit der Methode execute() des Statement-Objekts wird eine SQL-Anweisung ausgeführt
+           //  execute() erzeugt eine Ergebnismenge, die mit Hilfe eines Objekts der Klasse ResultSet ausgelesen werden kann
+      //  ResultSet rs = stm.getResultSet();  Mit Methoden des ResultSet-Objekts können Daten gelesen und Java-
+            //    Variablen zugewiesen werden.
+
              ResultSet resultSet = statement.executeQuery("SELECT * FROM OOP2_SS23_G1_P1.SETTINGS")) {
+            //Tabelle in resultset gespeichert.
 
             while (resultSet.next()) {
                 pfad = resultSet.getString(typ);
@@ -59,7 +72,13 @@ public class SettingsList extends Observable {
             String sql = "INSERT INTO Settings (" + typ + ") VALUES (?)";
             PreparedStatement statement = connection.prepareStatement(sql);
 
+           // Müssen Host-Variablen in ein SQL-Statement eingefügt werden, empfiehlt sich die Verwendung eines PreparedStatement.
+               //     Dabei steht jedes ? für eine Variable, die mit setter- Methoden gesetzt werden.
+
             statement.setString(1, pfad);
+
+           // Sie schreiben Änderungen direkt in die Datenbank mit der Methode executeUpdate()
+            //der Klassen Statement oder PreparedStatement
 
             int rowsUpdated = statement.executeUpdate();
 
